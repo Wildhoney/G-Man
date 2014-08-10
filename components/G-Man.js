eb428aea3f77e164f692c36fab574e4c4471d805
Wildhoney <adam.timberlake@gmail.com>
2014-08-10 22:26:00 +0100

    Added basic error checking

--- components/G-Man.js
+++ components/G-Man.js
@@ -9,6 +9,14 @@
      */
     var args = require('minimist')(process.argv.splice(2));
 
+    // Determine if we should be showing the index instead.
+    if (args._.length === 0) {
+
+        // Invoke the indexer.
+        return require('./Indexer.js');
+
+    }
+
     /**
      * @property fileSystem
      * @type {Object}
@@ -39,8 +47,20 @@
      */
     var PrettyError = require('pretty-error');
 
+    // Begin to read the documentation file.
+    var filePath   = config.directory + '/' + args._[0] + '.md',
+        fileExists = fileSystem.existsSync(filePath);
+
+    // Ensure the documentation file exists.
+    if (!fileExists) {
+
+        var renderedError = new PrettyError().render(new Error('Documentation file "' + filePath + '" does not exist.'));
+        return console.log('\n' + renderedError);
+
+    }
+
     // Read the Markdown documentation based on the passed argument.
-    fileSystem.readFile(config.directory + '/' + args._[0] + '.md', function (error, data) {
+    fileSystem.readFile(filePath, function (error, data) {
 
         if (error) {
 
